c09ffcd7dcbe95ab20d98eaf50d75bd6a384ac0f
soret <karimovminds@gmail.com>
2023-05-13 20:53:53 +0400

    cleaned some code in project, next step is pagination

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,109 +1,120 @@
+// Libs
 import React, { useState } from "react";
 import { Octokit } from "octokit";
-
-import { Link } from "react-router-dom";
-
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+// Store components
 import { init } from "../redux/resultSlice";
 
+// Components
 import ErrMessage from "../components/message";
 
 function SearchBar() {
 	const dispatch = useDispatch();
 
 	// Хуки для рендера сообщения
-	// const [isErr, setIsErr] = useState(false);
-	// const [message, setMessage] = useState("");
+	const [isErr, setIsErr] = useState(false);
+	const [message, setMessage] = useState("");
 	const [search, setSearch] = useState("");
+	const navigate = useNavigate();
 
-	// // Сообщения с ошибками
-	// const noValid = "Error: enter a valid search.",
-	// 	noOwner = "Error: enter a user.",
-	// 	noRepo = "Error: repository doesn't exist.";
+	// Сообщения с ошибками
+	const noValid = "ERROR: enter a valid search.",
+		noOwner = "ERROR: enter a user.",
+		noRepo = "ERROR: repository doesn't exist.";
 
-	// // Валидация поиска
-	// const validateSearch = (e) => {
-	// 	setSearch(e.target.value);
+	function handleSearch(e) {
+		setSearch(e.target.value);
+	}
 
-	// 	switch (true) {
-	// 		case search.indexOf("/") === -1 || search.length === 0:
-	// 			setIsErr(true);
-	// 			setMessage(noValid);
-	// 			break;
+	// Валидация поиска
+	const validateSearch = (e) => {
+		switch (true) {
+			case search.indexOf("/") === -1 || search.length === 0:
+				setIsErr(true);
+				setMessage(noValid);
+				break;
 
-	// 		case search.indexOf("/") === 0:
-	// 			setIsErr(true);
-	// 			setMessage(noOwner);
-	// 			break;
+			case search.indexOf("/") === 0:
+				setIsErr(true);
+				setMessage(noOwner);
+				break;
 
-	// 		case search.indexOf("/") === search.length - 1 ||
-	// 			(search.indexOf("/") === search.length - 1 && search.substring(search.indexOf("/"), search.length) === ""):
-	// 			setIsErr(true);
-	// 			setMessage(noRepo);
-	// 			break;
+			case search.indexOf("/") === search.length - 1 ||
+				(search.indexOf("/") === search.length - 1 && search.substring(search.indexOf("/"), search.length) === ""):
+				setIsErr(true);
+				setMessage(noRepo);
+				break;
 
-	// 		default:
-	// 			setIsErr(false);
-	// 			setMessage("");
-	// 			break;
-	// 	}
-	// };
+			default:
+				getRepo();
+				navigate("/search", { replace: false });
+				break;
+		}
+
+		setTimeout(() => {
+			setIsErr(false);
+			setMessage("");
+		}, 5000);
+	};
+
+	// Переход на страницу результатов
+	const moveToSearch = () => {};
 
 	// Отправка запроса
-	const GITHUB_TOKEN = "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";
-	const octokit = new Octokit({
-		auth: GITHUB_TOKEN,
-	});
+	async function getRepo() {
+		const GITHUB_TOKEN = "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";
+		const octokit = new Octokit({
+			auth: GITHUB_TOKEN,
+		});
 
-	async function getRepo(e) {
 		let owner = search.substring(0, search.indexOf("/")),
-			repo = search.substring(search.indexOf("/") + 1, search.length);
+			repo = search.substring(search.indexOf("/") + 1, search.length),
+			storeResult = [],
+			page = 1;
 
 		try {
-			const request = await octokit.request("GET /repos/{owner}/{repo}/forks", {
-				owner: owner,
-				repo: repo,
-				per_page: 50,
-				page: 1,
-			});
+			do {
+				if (page === 3) break;
 
-			const storeResult = request.data.map((item) => {
-				return { title: item.full_name, author: item.owner.login, stars: item.stargazers_count, link: item.html_url };
-			});
+				var request = await octokit.request("GET /repos/{owner}/{repo}/forks", {
+					owner: owner,
+					repo: repo,
+					per_page: 100,
+					page: page,
+				});
+				page++;
+
+				request.data.map((item) => {
+					return storeResult.push({ title: item.name, author: item.owner.login, stars: item.stargazers_count, link: item.html_url });
+				});
+			} while (request.data.length === 100);
+
 			dispatch(init(storeResult));
+			moveToSearch();
 		} catch (err) {
 			console.log(err);
 		}
 	}
 
-	const validateSearch = (e) => {
-		setSearch(e.target.value);
-	};
-
 	return (
-		<form className="search">
-			<input
-				type="text"
-				className="search__input"
-				id="searchBar"
-				placeholder="Найти репозиторий..."
-				value={search}
-				onInput={validateSearch}
-				onChange={validateSearch}
-			/>
-			<button type="submit" onClick={getRepo} className="search__button">
-				<Link to="/search">
-					<svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<>
+			<form className="search">
+				<input type="text" className="search__input" id="searchBar" placeholder="Найти репозиторий..." value={search} onChange={handleSearch} />
+				<button type="button" onClick={validateSearch} className="search__button">
+					<svg viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
 						<path d="M16.9166 17.4167L22.75 23.25" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
 						<path
 							d="M18.8611 12.5556C18.8611 16.3142 15.8142 19.3611 12.0556 19.3611C8.29695 19.3611 5.25 16.3142 5.25 12.5556C5.25 8.79695 8.29695 5.75 12.0556 5.75C15.8142 5.75 18.8611 8.79695 18.8611 12.5556Z"
 							strokeWidth="2"
 						/>
 					</svg>
-				</Link>
-			</button>
-			{/* <ErrMessage show={isErr} errText={message} /> */}
-		</form>
+				</button>
+			</form>
+			<ErrMessage show={isErr} errText={message} />
+		</>
 	);
 }
+
 export default SearchBar;
--- src/components/table.jsx
+++ src/components/table.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { add } from "../redux/resultSlice";
+import { useSelector } from "react-redux";
 
 import TableCell from "../components/tableCell";
 
@@ -18,25 +17,23 @@ function Table() {
 						<th className="table__heading">#</th>
 						<th className="table__heading">Название</th>
 						<th className="table__heading">Владелец</th>
-						<th className="table__heading">Кол-во звезд</th>
+						<th className="table__heading">Звезды</th>
 						<th className="table__heading">Избранное</th>
 						<th className="table__heading">Ссылка</th>
 					</tr>
 				</thead>
 				<tbody>
-					{storeResult.map((item, index) => {
-						return (
-							<TableCell
-								key={index + 1}
-								id={index + 1}
-								name={item.title}
-								author={item.author}
-								stars={item.stars}
-								favorite="Да"
-								link={item.link}
-							/>
-						);
-					})}
+					{storeResult.map((item, index) => (
+						<TableCell
+							key={index + 1}
+							id={index + 1}
+							name={item.title}
+							author={item.author}
+							stars={item.stars}
+							favorite="Да"
+							link={item.link}
+						/>
+					))}
 				</tbody>
 			</table>
 		);
